5ff15413c53175b838824aab1581bb030c6118b6
Xavier Graterol <xavier.graterol@gmail.com>
2015-07-10 11:37:09 -0430

    FIX - FECHA OTORGACION Y FECHA VENCE

--- db/migrate/20150609121755_create_unidad_manejos.rb
+++ db/migrate/20150609121755_create_unidad_manejos.rb
@@ -5,8 +5,8 @@ class CreateUnidadManejos < ActiveRecord::Migration
       t.string :nombre, null: false, limit: 64
       t.string :abreviado, null:false, limit: 12
       t.string :nro_providencia, null:false, limit: 12
-      t.date :fecha_otorgamiento
-      t.date :fecha_vencimiento
+      t.date :fecha_otorgacion
+      t.date :fecha_vence
       t.decimal :area, scale: 2, precision: 12
       t.text :ubicacion
       t.text :descripcion
